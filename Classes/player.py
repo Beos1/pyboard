13fd1e637ac15f4c66061c16c6863341c60497ab
Tom <Garnett1995@hotmail.com>
2020-01-29 18:54:01 -0800

    Starting work on GUI and player/board setup

--- Classes/player.py
+++ Classes/player.py
@@ -1,8 +1,22 @@
 class player:
     def __init__(self, playerNumber):
-        this.playerNumber = playerNumber
-        this.possition1 = setHome(playerNumber)
-        this.possition2 = setHome(playerNumber)
-        this.possition3 = setHome(playerNumber)
-        this.possition4 = setHome(playerNumber)
+        self.playerNumber = playerNumber
+        self.possition1 = setHome(playerNumber)
+        self.possition2 = setHome(playerNumber)
+        self.possition3 = setHome(playerNumber)
+        self.possition4 = setHome(playerNumber)
  
+
+def setHome(playerNumber):
+    if playerNumber == 1:
+        return 112
+    elif playerNumber == 2:
+        return 115
+    elif playerNumber == 3:
+        return 113
+    elif playerNumber == 4:
+        return 116
+    elif playerNumber == 5:
+        return 114
+    else:
+         return 117
--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-import random
+from Classes.player import player
+from Classes.dice import rollDie
+from Classes.board import board
+from appJar import gui
 playerCount = 0
-while(0>=playerCount or playerCount>6):
+players = []
+while(1>=playerCount or playerCount>6):
     playerCount = int(input("How many Players(1-6): "))
-print(playerCount)
-print(random.randint(1,6))
+newGame = board()
+x= 1
+while(x<playerCount):
+    players.append(player(x))
+    x = x+1
+    print(x)
+isThereAWinner = False
+currentRoll = 0
+while(isThereAWinner==False):
+    for player in players:
+        currentRoll = rollDie()
+        print("You Rolled: ")
+        print(currentRoll)
+        isThereAWinner=True
+print("The winner is player number ")
+winner = "me"
+print( winner)
+app = gui()
+app.addLabel("title", "Welcome to appJar")
+app.setLabelBg("title", "red")
+app.go()
